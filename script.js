38dd245f55c905abf71be8460a0d976498b04ed1
Sasha5026 <skrstovic@DESKTOP-DK6OLV5.>
2025-04-23 16:32:13 +0200

    Design complete. Add functions to buttons.

---
+++ script.js
@@ -0,0 +1,37 @@
+let firstNum = 7;
+let secondNum = 14;
+let operator = '*'; 
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) return 'Error! Cannot divide by zero!';
+    return a / b;
+}
+
+function operate(num1, num2, operator) {
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return 'Error! Unknown operator!';
+    }
+}
+
+console.log(operate(firstNum, secondNum, operator));
